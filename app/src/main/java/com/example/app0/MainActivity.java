e0f6ad830fcfca37c5a726fa20b03f6679eb56f3
Miguel <miguel@equalit.ie>
2022-05-05 22:36:11 -0500

    Fetch the URL after Ouinet client is started

--- app/src/main/java/com/example/app0/MainActivity.java
+++ app/src/main/java/com/example/app0/MainActivity.java
@@ -12,9 +12,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView myWebView = new WebView(this);
-        setContentView(myWebView);
-        myWebView.loadUrl("https://censorship.no/en/index.html");
 
         Config config = new Config.ConfigBuilder(this)
                 .setCacheType("bep5-http")
@@ -24,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
         ouinet = new Ouinet(this, config);
         ouinet.start();
+
+        WebView myWebView = new WebView(this);
+        setContentView(myWebView);
+        myWebView.loadUrl("https://censorship.no/en/index.html");
     }
 }
